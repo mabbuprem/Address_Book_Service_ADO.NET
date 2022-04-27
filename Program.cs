5ba5291909284d044595e39c39ddaa5563b83246
mabbuprem <mabbupremkumar@gmail.com>
2022-04-27 16:18:10 +0530

    [prem] FEATURE ADDED:Retrieving Data from AddressBookService Database

--- Program.cs
+++ Program.cs
@@ -8,7 +8,19 @@ namespace AddressBookSystem_ADO.NET
         {
             Console.WriteLine("Welcome to AddressBookSystem using Ado.net");
             AddressBookRepo addressBookRepo = new AddressBookRepo();
+            AddressBookModel addressBookModel = new AddressBookModel();
+            addressBookModel.FirstName = "PREM";
+            addressBookModel.LastName = "KUMAR";
+            addressBookModel.Address = "Salanuthala,konakana metla";
+            addressBookModel.City = "Ongole";
+            addressBookModel.State = "AndhraPradesh";
+            addressBookModel.Zip = 523241;
+            addressBookModel.PhoneNo = 7206594149;
+            addressBookModel.Email = "mabbupremkumar@gmail.com";
+            addressBookModel.AddressBookName = "friend address book";
+            addressBookModel.AddressBookType = "Friend";
             addressBookRepo.checkConnection();
+            addressBookRepo.addNewContactToDataBase(addressBookModel);
         }
     }
 }
